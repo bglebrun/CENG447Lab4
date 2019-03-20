0e7b76ee4cc1dcd5c5374c5be707715efbdc18ea
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-03-19 21:18:31 -0600

    Some uart stuff, strings file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "strings.h"
 
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
@@ -16,7 +17,13 @@ ISR(USART_TX_vect){
 }
 
 int main() {
-  sei();                                              // set global interrupts
+  // UART init code
+  UBRR0H = (BAUD_PRESCALE>>8);
+  UBRR0L = BAUD_PRESCALE;
+  UCSR0B = (1<<RXEN0) | (1<<RXCIE0);
+  UCSR0C |= (1<<UCSZ00 | (1 << UCSZ01));
+  // set global interrupts
+  sei();
   while (true) {
     
   }
---
+++ strings.h
@@ -0,0 +1,6 @@
+#ifndef _STRINGS_H_
+#define _STRINGS_H_
+
+static const char invalidMsg[] = "INVALID PIN, Command structure: ' READ/ (WRITE) PIN_NUM (HIGH|LOW)'";
+
+#endif
