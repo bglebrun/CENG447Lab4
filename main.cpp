68566a025757aa00a39a8002e6b006a6924b82a4
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-03-19 22:07:47 -0600

    simple UART rx

--- main.cpp
+++ main.cpp
@@ -2,14 +2,29 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <string.h>
 #include "strings.h"
 
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
-ISR(USART_RX_vect){
-  char inByte;
+char message[32];
+int i = 0;
 
+ISR(USART_RX_vect){
+  char inByte = UDR0;
+  // processing goes here
+  if (inByte=='\n') {
+    i = 0;
+    // process string here
+    
+    // clear message buffer when done
+    memset(message, 0, sizeof message);
+  } else {
+    // continue to write to a fifo buffer
+    message[i++] = inByte;
+    inByte = 0;
+  }
 }
 
 ISR(USART_TX_vect){
