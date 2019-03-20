7368dfa22c53d60ce7c3ed334ee2c0e41594bd84
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-03-19 22:33:10 -0600

    Making includes file

---
+++ includes.h
@@ -0,0 +1,12 @@
+#ifndef _INLCUDES_H_
+#define _INCLUDES_H_
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <string.h>
+#include "strings.h"
+
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+#endif
--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
-#define F_CPU 16000000
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
-#include <string.h>
-#include "strings.h"
-
-#define USART_BAUDRATE 9600
-#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+#include "includes.h"
 
 char message[32];
 int i = 0;
+
+int main() {
+  // UART init code
+  UBRR0H = (BAUD_PRESCALE>>8);
+  UBRR0L = BAUD_PRESCALE;
+  UCSR0B = (1<<RXEN0) | (1<<RXCIE0);
+  UCSR0C |= (1<<UCSZ00 | (1 << UCSZ01));
+  // set global interrupts
+  sei();
+  while (true) {
+    
+  }
+  return 1;
+}
 
 ISR(USART_RX_vect){
   char inByte = UDR0;
@@ -29,18 +35,4 @@ ISR(USART_RX_vect){
 
 ISR(USART_TX_vect){
 
-}
-
-int main() {
-  // UART init code
-  UBRR0H = (BAUD_PRESCALE>>8);
-  UBRR0L = BAUD_PRESCALE;
-  UCSR0B = (1<<RXEN0) | (1<<RXCIE0);
-  UCSR0C |= (1<<UCSZ00 | (1 << UCSZ01));
-  // set global interrupts
-  sei();
-  while (true) {
-    
-  }
-  return 1;
 }
